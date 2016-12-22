7b9b399bd96968ffe6c529d6260c2aa80720e7a8
Brian <bwatk001@odu.edu>
2016-12-21 19:22:07 -0500

    Commited by Brian

--- includes/morningQuestionsScripts.js
+++ includes/morningQuestionsScripts.js
@@ -18,66 +18,115 @@ function morningQuestion(
 			this.minutesToFallAsleep = minutesToFallAsleep;
 			this.didWakeDuringTheNight = didWakeDuringTheNight;
 			this.minutesToFallBackToSleep = minutesToFallBackToSleep;
+			this.howDidYouFeel = howDidYouFeel;
 			this.anythingBotherYourSleep = anythingBotherYourSleep;
 
 			this.setBedTime = function setBedTime() {
-				var setBedTime = document.getElementById('inputBedTimeId').value;
+				var setBedTime = document.getElementById('inputBedTimeId').value; // get data
 				this.bedTime = setBedTime;
 			}
 
+
 			this.setWakeTime = function setWakeTime() {
-				var setWakeTime = document.getElementById('inputWakeTimeId').value;
+				var setWakeTime = document.getElementById('inputWakeTimeId').value; // get data
 				this.wakeTime = setWakeTime;
 			}
 
 			this.setProblemsFallingAsleep = function setProblemsFallingAsleep() {
-				var setProblemsFallingAsleep = document.getElementById('inputProblemsFallingAsleepId').value;
+				var setProblemsFallingAsleep = document.getElementById('inputProblemsFallingAsleepId').value; // get data
 				this.problemsFallingAsleep = setProblemsFallingAsleep;
 			}
 
 			this.setMinutesToFallAsleep = function setMinutesToFallAsleep() {
-				var setMinutesToFallAsleep = document.getElementById('inputMinutesToFallAsleepId').value;
+				var setMinutesToFallAsleep = document.getElementById('inputMinutesToFallAsleepId').value; // get data
 				this.minutesToFallAsleep = setMinutesToFallAsleep;
 			}
 			this.setDidWakeDuringTheNight = function setDidWakeDuringTheNight() {
-				var setDidWakeDuringTheNight = document.getElementById('inputDidWakeDuringTheNightId').value;
+				var setDidWakeDuringTheNight = document.getElementById('inputDidWakeDuringTheNightId').value; // get data
 				this.didWakeDuringTheNight = setDidWakeDuringTheNight;
 			}
 
 			this.setMinutesToFallBackToSleep = function setMinutesToFallBackToSleep() {
-				var setMinutesToFallBackToSleep = document.getElementById('inputMinutesToFallBackToSleepId').value;
+				var setMinutesToFallBackToSleep = document.getElementById('inputMinutesToFallBackToSleepId').value; // get data
 				this.minutesToFallBackToSleep = setMinutesToFallBackToSleep;
 			}
+			
+
+			this.setHowDidYouFeel = function setHowDidYouFeel(choice) {
+
+				this.howDidYouFeel = choice;
+			}
+
+
+
+
+
 
 			this.setAnythingBotherYourSleep = function setAnythingBotherYourSleep() {
-				var setAnythingBotherYourSleep = document.getElementById('inputAnythingBotherYourSleepId').value;
+				var setAnythingBotherYourSleep = document.getElementById('inputAnythingBotherYourSleepId').value; // get data
 				this.anythingBotherYourSleep = setAnythingBotherYourSleep;
 			}
-
-			this.printAll = function printDetails() {
-
-				this.setBedTime();
-				this.setWakeTime();
-				this.setProblemsFallingAsleep();
-				this.setMinutesToFallAsleep();
-				this.setDidWakeDuringTheNight();
-				this.setMinutesToFallBackToSleep();
-				this.setAnythingBotherYourSleep();
-
-
-
-				document.getElementById('demo').innerHTML = this.bedTime + "</br>" + 
-				" " + this.wakeTime + "</br>" + 
-				" " + this.problemsFallingAsleep + "</br>" + 
-				" " + this.minutesToFallAsleep + "</br>" + 
-				" " + this.didWakeDuringTheNight +  "</br>" + 
-				" " + this.minutesToFallBackToSleep + "</br>" + 
-				" " + this.anythingBotherYourSleep +  "</br>" + 
-				"</br>"
-				;
-			}
-
 		}
-var questions = new morningQuestion(); // create questions instance
+
+
+
+
+		function checkQuestion1() { // Bed time and wake time
+			questions.setBedTime(document.getElementById('inputBedTimeId').value);
+			questions.setWakeTime(document.getElementById('inputWakeTimeId').value);
+		}
+
+		function checkQuestion2() { // problems fall asleep, how many minutes
+			questions.setProblemsFallingAsleep(document.getElementById('inputProblemsFallingAsleepId').checked);
+			questions.setMinutesToFallAsleep(document.getElementById('inputMinutesToFallAsleepId').value);
+		}
+		function checkQuestion3() { // wake up at night, how many minutes to fall asleep
+			questions.setDidWakeDuringTheNight(document.getElementById('inputDidWakeDuringTheNightId').checked);
+			questions.setMinutesToFallBackToSleep(document.getElementById('inputMinutesToFallBackToSleepId').value);
+		}
+
+		function checkQuestion4(choice) { // how did you feel
+			var x = choice;
+			questions.setHowDidYouFeel(choice);
+
+			switch (x){
+				case 1:
+				document.getElementById('somewhatSleepyId').checked = false;
+				document.getElementById('alertId').checked = false;
+				break;
+				case 2:
+				document.getElementById('sleepyId').checked = false;
+				document.getElementById('alert').checked = false;
+				break;
+				case 3:
+				document.getElementById('somewhatSleepyId').checked = false;
+				document.getElementById('sleepyId').checked = false;
+				break;
+			}
+		}
+		function checkQuestion5() { // anything bother the sleeper last night
+
+			document.getElementById('noiseId').checked
+			document.getElementById('lightId').checked
+			document.getElementById('worryId').checked
+			document.getElementById('tempId').checked
+			document.getElementById('noneId').checked
+
+  			var x = x ? 1 : 0; // Ternary (conditional ) operator
+  			var y = y ? 1 : 0; // variableName = (conditiona) ? value1:value2
+  			var z = z ? 1 : 0; // value1 if true, value2 if false
+
+  			var result = "" + x + y + z ;
+  			return result ; 
+  		}
+  	}
+
+
+  var questions = new morningQuestion(); // create questions instance
+
+
+
+
+
 
 
