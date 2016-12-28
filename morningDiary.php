8a76338bedf4445500cf55a17f56b67d18e1e3dc
Brian <bwatk001@odu.edu>
2016-12-27 20:12:20 -0500

    Commited by Brian

--- morningDiary.php
+++ morningDiary.php
@@ -205,49 +205,78 @@ var questionBank = [qs1, qs2, qs3, qs4, qs5];
 
 
 
-      <!-- End of BootStrap DatePicker Code --> 
+
       <script type="text/javascript">
         var elem = document.querySelector('.js-switch');
         var init = new Switchery(elem);
       </script>
 
+
+
+
+
+      <!-- Navigation buttons for the Morning Questions --> 
       <div class="questionButtonDiv">
         <label id="backButton" class="btn btn-default">Back</label>
         <label id="nextButton" class="btn btn-default">Next</label>
       </div>
 
 
-    </div>
+
+      <form  id="questions">
+        <p id="questionDisplay"> </p>  <!-- Displays the questions here -->
+      </form>
 
 
-    <form  id="questions">
-      <p id="questionDisplay"> </p>
-    </form>
 
 
-    <button onclick="viewAll()">button</button>
+
+
+      <!-- Test Button - look at data -->
+      <!-- <button onclick="viewAll()">button</button> --> 
 
   <script> // pages through the questions
 
    document.getElementById("questionDisplay").innerHTML = questionBank[0];
    var ctr = 0; // start at first question
+   document.getElementById("backButton").style.display = "none";
 
    $(document).ready(function(){
+
     $("#nextButton").click(function() {  // NEXT Button
       if(ctr < 4) {
        ctr++;
        document.getElementById("questionDisplay").innerHTML = questionBank[ctr];
        updateValues(); // updates the form with the appropriate data
        if (ctr >= 4) {ctr = 4;}
-     }
-   })
+       // ************************************************************************
+       if (ctr == 4) { 
+        document.getElementById("nextButton").style.display = "none"; 
+      } else { 
+        document.getElementById("nextButton").style.display = "inline";
+        document.getElementById("backButton").style.display = "inline"; 
+      }
+      // ************************************************************************** 
+    }
+  })
 
-    $("#backButton").click(function() {
+    $("#backButton").click(function() { // BACK Button
       if( ctr >= 1){
        ctr--;
-       document.getElementById("questionDisplay").innerHTML = questionBank[ctr];
-       updateValues(); // updates the form with the appropriate data
        if (ctr == 0) {ctr = 0;}
+
+       if (ctr == 0) { 
+        document.getElementById("backButton").style.display = "none";
+        document.getElementById("nextButton").style.display = "inline"; 
+      } else { 
+        document.getElementById("nextButton").style.display = "inline";
+        document.getElementById("backButton").style.display = "inline";
+      }
+
+
+      document.getElementById("questionDisplay").innerHTML = questionBank[ctr];
+       updateValues(); // updates the form with the appropriate data
+       
      }
    })
   });
@@ -258,6 +287,21 @@ var questionBank = [qs1, qs2, qs3, qs4, qs5];
 
 
 </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
