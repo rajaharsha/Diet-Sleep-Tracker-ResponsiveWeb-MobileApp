d91552c22263aad8a2c66bf32dd8e5d6787e3468
Brian <bwatk001@odu.edu>
2017-04-07 18:13:09 -0400

    Commited by Brian

--- menu.php
+++ menu.php
@@ -7,32 +7,37 @@
 ?>
 
 
+<div class="container-fluid">
 
-<div class="slideDown btn group text-center">
-      <!-- Example row of columns -->
-        <div class="col-md-4">
-            <br>
-            <button type="button" class="btn btn-primary btn-lg btn-block btn-info" onclick="location.href='diary.php';"> 
+
+                  <div class="col-xs-1 col-lg-3"></div>
+                  <div class="col-xs-10 col-md-4 col-lg-2">
+                  <button type="button" class="btn btn-primary btn-lg btn-block btn-info" onclick="location.href='diary.php';">
                   <img src="img/Sleep_Diary.png" class="center-block" alt="CoachZ" width="150" height="150">
-            </button>
-            <br>
-        </div>
-        <div class="col-md-4">
+                  </button>
+                  <br />
+                  </div>
 
-            <br>
-            <button type="button" class="btn btn-primary btn-lg btn-block btn-info" onclick="location.href='coachz_tips.php';">  
+
+                  <div class="col-xs-1"></div>
+                  <div class="col-xs-10 col-md-4 col-lg-2">
+                  <button type="button" class="btn btn-primary btn-lg btn-block btn-info" onclick="location.href='coachz_tips.php';">  
                   <img src="img/CoachZ_Tips.png" class="center-block" alt="CoachZ" width="150" height="150">
-            </button>
-            <br>
-        </div>  
-        <div class="col-md-4">
-            <br>
-            <button type="button" class="btn btn-primary btn-lg btn-block btn-info" onclick="location.href='sleep_tracker.php';"> 
+                  </button>
+                  <br />
+                  </div>
+
+
+                  <div class="col-xs-1"></div>
+                  <div class="col-xs-10 col-md-4 col-lg-2">
+                  <button type="button" class="btn btn-primary btn-lg btn-block btn-info" onclick="location.href='sleep_tracker.php';"> 
                   <img src="img/Sleep_Tracker.png" class="center-block" alt="CoachZ" width="150" height="150">
-            </button>
-            <br>
-        </div>
-</div>            
+                  </button>
+                  </div>
+      </div>
+
+
+           
 
 <br><br>
 <?php require_once("./includes/footer.php"); ?>
